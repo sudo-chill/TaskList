3d28b6febc2e489987a3e1e670a49fc70b712ee2
Greg Martin <00ganon@gmail.com>
2019-09-23 13:12:05 -0700

    initial commit

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './TaskList.css';
 
-function App() {
+function TaskList() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="TaskList">
+      <header className="TaskList-header">
+        <img src={logo} className="TaskList-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="TaskList-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -23,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default TaskList;
